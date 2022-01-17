45b3aad450ffc1200bbc9f9bdc91977b2868ca8f
toufiqulhoque <xtoufiqxx@gmail.com>
2022-01-17 12:00:51 +0600

    Search Result page done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
+import SearchPage from './Pages/SearchPage/SearchPage';
 import Footer from './Shared/Header/Footer/Footer';
 import Header from './Shared/Header/Header';
 
@@ -8,10 +10,22 @@ function App() {
   return (
     //BEM
     <div className="app">
-      <Header></Header>
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route exact path='/search'>
+            <SearchPage></SearchPage>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </BrowserRouter>
 
-      <Home></Home>
-      <Footer></Footer>
+
+
+
     </div>
   );
 }
